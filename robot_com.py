fb6aafd978bc3939830749723b00921f029559a1
John Zhang <46901343+jyzhang111@users.noreply.github.com>
2019-10-22 21:51:35 -0400

    Update robot_com.py

--- robot_com.py
+++ robot_com.py
@@ -4,83 +4,42 @@ Allow user to control robot using natural English language via command line IO
 
 import random
 import re
-import time
 from client import DroidClient
-from matplotlib import colors
 from r2d2_commands import *
 
 __author__ = "Zhenghua (Calvin) Chen"
+__author__ = "John Zhang"
 
 # Uncomment the code below for command line IO, remember to recomment if you are using voice IO
 
 def main():
-    categories = ["state", "direction", "light", "animation", "head", "grid"]
-    indexToTrainingSentence = {}
-    i = 0
-    for category in categories:
-        sentenceType = globals()[category + "Sentences"]
-        for sentence in sentenceType:
-            indexToTrainingSentence[i] = (sentence, category)
-            i += 1
-    
-    sentenceEmbeddings = np.zeros((len(indexToTrainingSentence), vectors.dim))
-
-    for i in range(len(indexToTrainingSentence)):
-        sentence = indexToTrainingSentence[i][0]
-        sentenceEmbedding = calcPhraseEmbedding(sentence)
-
-    sentenceEmbeddings[i, :] = sentenceEmbedding
+    print("Welcome to CommandDroid, where we will try our best to understand what you want our R2D2 to do.")
+    print("In this environment, type 'exit', 'quit', 'bye', or 'goodbye' to quit.")
+    print("***********************************************")
 
     # Replace this with your own robot serial ID
-    robot = Robot("D2-F75E")
-    while True:
-        print("\nEnter your instruction: ")
-        cmd = input().lower()
-        if re.search(r"\b(exit|quit|bye|goodbye)\b", cmd, re.I):
+    robot = AnimateR2('D2-152E', 0.70, False)
+
+    while(True):
+        command = input("You: ")
+        command = command.strip()
+        if re.search(r"\b(exit|quit|bye|goodbye)\b", command, re.I):
             break
 
-        tokens = re.split("[^a-zA-Z]", cmd)
-        if re.search(r"\b(light|lights|led|leds|backlight|backlights)\b", cmd, re.I):
-            if re.search(r"\b(front|forward)\b", cmd, re.I):
-                robot.set_front_LED_color(tokens)
-            elif re.search(r"\b(back|rear|backlight|backlights)\b", cmd, re.I):
-                robot.set_back_LED_color(tokens)
-            else:
-                robot.set_front_LED_color(tokens)
-                robot.set_back_LED_color(tokens)
-        else:
-            flag = False
-            for token in tokens:
-                if token in {"up", "forward", "ahead", "straight"}:
-                    robot.roll("up")
-                    flag = True
-                elif token in {"down", "back"}:
-                    robot.roll("down")
-                    flag = True
-                elif token in {"left", "right"}:
-                    robot.roll(token)
-                    flag = True
-                elif token in {"dance", "move", "moves"}:
-                    robot.animate()
-                    flag = True
-                elif token in {"sing", "sound", "sounds", "noise", "noises"}:
-                    robot.play_sound()
-                    flag = True
-            if flag:
-                commandEmbedding = calcPhraseEmbedding(cmd)
-
-                closestSentences = rankSentences(commandEmbedding, sentenceEmbeddings)
-                if cosineSim(commandEmbedding, sentenceEmbeddings[closestSentences[0], :]) < 0.85:
-                    print(robot.name + ": I could not understand your command.")
-                    continue
-
-                commandType = getCommandType(categories, closestSentences, indexToTrainingSentence)
-                result = getattr(robot, commandType + "Parser")(cmd)
-                if result:
-                    print(robot.name + ": Done executing "  + commandType + " command.")
-                else:
-                    print(robot.name + ": I could not understand your " + commandType + " command.")
-
+        flag = False
+        tokens = re.split("[^a-zA-Z]", command.lower())
+        if len(tokens) == 0:
+            print("Please type something!")
+            continue
+        for token in tokens:
+            if token in {"dance", "move", "moves"}:
+                robot.animate()
+                flag = True
+            elif token in {"sing", "sound", "sounds", "noise", "noises"}:
+                robot.play_sound()
+                flag = True
+        if not flag:
+            robot.inputCommand(command)
     robot.disconnect()
 
 if __name__ == "__main__":
